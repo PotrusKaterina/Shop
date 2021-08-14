5f47c579af2bc4b96fbbfbe0d5d4f6054ef0a43a
Administrator <kpotr@softserveinc.com>
2021-08-15 01:09:34 +0200

    go to sleep

--- Shop/app/navigation/stackNavigation.tsx
+++ Shop/app/navigation/stackNavigation.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {ARCHIVED, MAIN} from '../config/routes';
+import {ARCHIVED} from '../config/routes';
 import ArchivedScreen from '../views/archived';
-import MainScreen from '../views/main';
 
 const Stack = createStackNavigator<any>();
 
 const StackNavigation = () => {
   return (
-    <Stack.Navigator>
-      {/* <Stack.Screen name={ARCHIVED} component={ArchivedScreen} /> */}
-      <Stack.Screen name={MAIN} component={MainScreen} />
-    </Stack.Navigator>
+    <>
+    
+      <Stack.Screen name={ARCHIVED} component={ArchivedScreen} />
+    </>
   );
 };
 
